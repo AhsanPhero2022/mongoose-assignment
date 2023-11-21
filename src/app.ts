e1b8f955e6575202bff250268cd38f3f2e1deb19
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2023-11-21 23:34:34 +0600

    done ts student features

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 const app: Application = express();
-
-
 
 // parsers
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  
+// api/v1/students/create-student
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+app.get('/', (req: Request, res: Response) => {
   res.send('hello world');
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,17 @@
-import app from "./app";
-import config from "./app/config";
+import app from './app';
+import config from './app/config';
 
-import mongoose from "mongoose";
-
+import mongoose from 'mongoose';
 
 async function main() {
-   try{
+  try {
     await mongoose.connect(config.database_url as string);
     app.listen(config, () => {
-        console.log(`Example app listening on port ${config.port}`)
-      })
-   }catch(error){
-    console.log(error)
-   }
-   
+      console.log(`Asia, Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
   }
+}
 
-
-main()
+main();
